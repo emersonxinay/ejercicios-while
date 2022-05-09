e7c41ad4017ce1c741e7c9b17a4ad1eb4d0ba99f
Emerson Espinoza <xinayespinoza@gmail.com>
2022-05-09 15:23:28 -0400

    Ejercicios basicos

---
+++ mayor_de_edad.rb
@@ -0,0 +1,8 @@
+# comprobar si es o no es mayor de edad 
+
+a = ARGV[0].to_i 
+if a >= 18 
+  puts "Es mayor de edad"
+else
+  puts "Es menor de edad "
+end
---
+++ ordenar_arreglo.rb
@@ -0,0 +1,5 @@
+a = [1, 2, 4, 9]
+e = []
+a.each do |x|
+  print e<<x
+end
